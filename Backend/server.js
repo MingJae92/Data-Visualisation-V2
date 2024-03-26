b860adabb997b59a9237cc8f54196ee183201e62
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-03-26 17:16:07 +0000

    server updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,21 +1,12 @@
-import express from "express"
-import dotenv from "dotenv";
-import path from "path";
-
-const __filename = new URL(import.meta.url).pathname;
-const __dirname = path.dirname(__filename);
-
-dotenv.config({ path: path.resolve(__dirname, "..", "config", ".env") });
+import express from "express";
 
 const app = express();
-const PORT = process.env.SERVER_PORT || 5000;
-
-
+const PORT = 9000;
 
 try {
-    app.listen((PORT), () => {
-      console.log(`Server now connected to port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("Error starting the server:", error.message);
-  }
+  app.listen(PORT, () => {
+    console.log(`Server now connected to port ${PORT}`);
+  });
+} catch (error) {
+  console.error("Error starting the server:", error.message);
+}
