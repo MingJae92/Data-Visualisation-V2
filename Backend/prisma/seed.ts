217970ca7240e7c6d627573c7ac4fa44ae5e7838
Ming Chi <wong_mingchi@yahoo.co.uk>
2024-04-03 16:21:31 +0100

    Seed.ts updated

--- Backend/prisma/seed.ts
+++ Backend/prisma/seed.ts
@@ -3,26 +3,12 @@ import { createReadStream } from "fs";
 import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
-let rowNumber = 0; // Counter variable to keep track of the row number
-
 db.$connect();
 
 createReadStream("data.csv")
-  .pipe(
-    parse({})
-  )
+  .pipe(parse({}))
   .on("data", async (row) => {
-    rowNumber++; // Increment the row number for each new row
-
-    console.log("Row data:", row); // Log row data for debugging
-
-    // Check if the row has the expected number of fields (13 fields)
-    if (row.length !== 13) {
-      console.error(`Invalid number of fields in row ${rowNumber}:`, row);
-      return; // Skip processing this row
-    }
-
-    const {
+    const [
       date,
       retailer,
       ean,
@@ -36,71 +22,68 @@ createReadStream("data.csv")
       base_price,
       shelf_price,
       promoted_price,
-    } = row;
+    ] = row;
 
+    const { id: category_id } = await db.category.upsert({
+      create: {
+        name: category,
+      },
+      where: {
+        name: category,
+      },
+      update: {
+        name: category,
+      },
+    });
+
+    const { id: manufacturer_id } = await db.manufacturer.upsert({
+      create: {
+        name: manufacturer,
+      },
+      where: {
+        name: manufacturer,
+      },
+      update: {
+        name: manufacturer,
+      },
+    });
+
+    const { id: retailer_id } = await db.retailer.upsert({
+      create: {
+        name: retailer,
+      },
+      where: {
+        name: retailer,
+      },
+      update: {
+        name: retailer,
+      },
+    });
+
+    const { id: product_id } = await db.product.upsert({
+      create: {
+        ean,
+        brand,
+        product_title,
+        image,
+        manufacturer_id,
+        category_id,
+      },
+      where: {
+        ean,
+      },
+      update: {
+        ean,
+        brand,
+        product_title,
+        image,
+        manufacturer_id,
+        category_id,
+      },
+    });
     try {
-      const { id: categoryId } = await db.category.upsert({
-        create: {
-          name: category,
-        },
-        where: {
-          name: category,
-        },
-        update: {
-          name: category,
-        },
-      });
-
-      const { id: manufacturerId } = await db.manufacturer.upsert({
-        create: {
-          name: manufacturer,
-        },
-        where: {
-          name: manufacturer,
-        },
-        update: {
-          name: manufacturer,
-        },
-      });
-
-      const { id: retailerId } = await db.retailer.upsert({
-        create: {
-          name: retailer,
-        },
-        where: {
-          name: retailer,
-        },
-        update: {
-          name: retailer,
-        },
-      });
-
-      const { id: productId } = await db.product.upsert({
-        create: {
-          ean,
-          brand,
-          productTitle
-          image,
-          manufacturerId,
-          categoryId,
-        },
-        where: {
-          ean,
-        },
-        update: {
-          ean,
-          brand,
-          productTitle: product_title,
-          image,
-          manufacturerId,
-          categoryId,
-        },
-      });
-
-      const isOnPromotion = on_promotion.toUpperCase() === "TRUE";
-
-      if (isOnPromotion) {
-        const { id: promotionId } = await db.promotion.upsert({
+      if (on_promotion === "TRUE") {
+        const { id: promotion_id } = await db.promotion.upsert({
           create: {
             description: promotion_description,
           },
@@ -115,31 +98,31 @@ createReadStream("data.csv")
         await db.price.create({
           data: {
             date: new Date(date),
-            basePrice: parseFloat(base_price),
-            shelfPrice: parseFloat(shelf_price),
-            promotedPrice: parseFloat(promoted_price),
-            retailerId,
-            productId,
-            onPromotion: isOnPromotion,
-            promotionId,
+            base_price: parseFloat(base_price),
+            shelf_price: parseFloat(shelf_price),
+            promoted_price: parseFloat(promoted_price),
+            retailer_id,
+            product_id,
+            on_promotion: true,
+            promotion_id,
           },
         });
       } else {
         await db.price.create({
           data: {
             date: new Date(date),
-            basePrice: parseFloat(base_price),
-            shelfPrice: parseFloat(shelf_price),
-            promotedPrice: parseFloat(promoted_price),
-            retailerId,
-            productId,
-            onPromotion: isOnPromotion,
+            base_price: parseFloat(base_price),
+            shelf_price: parseFloat(shelf_price),
+            promoted_price: parseFloat(promoted_price),
+            retailer_id,
+            product_id,
+            on_promotion: false,
           },
         });
       }
     } catch (error) {
-      console.error(`Error processing row ${rowNumber}:`, row);
-      console.error(error);
+      console.log("Error on row: ", row);
+      console.log(error);
     }
   })
   .on("end", () => {
